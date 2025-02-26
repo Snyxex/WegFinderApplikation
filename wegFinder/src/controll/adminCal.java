66e5127e67e137c15f9749256c13d7511fe45550
snyxe <153672410+Snyxex@users.noreply.github.com>
2025-02-26 13:19:11 +0100

    main

---
+++ wegFinder/src/controll/adminCal.java
@@ -0,0 +1,33 @@
+package controll;
+
+public class adminCal {
+
+	
+ private void addUser() {
+	 
+ }
+ 
+ private void deleteUser() {
+	 
+ }
+ 
+ private void updateUser() {
+	 
+ }
+ 
+ private void addRoom() {
+	 
+ }
+ 
+ private void deleteRoom() {
+	 
+ }
+ 
+ private void lockRoom() {
+	 
+ }
+ private void lockCoridoor() {
+	 
+ }
+ 
+}
