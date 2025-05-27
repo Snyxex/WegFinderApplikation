e48de97fd959a0c9248727b19d2df228a7934c9f
snyxe <153672410+Snyxex@users.noreply.github.com>
2025-05-27 12:35:33 +0200

    main

--- wegfinderProject/src/com/wegapplikation/config/model/UserData.java
+++ wegfinderProject/src/com/wegapplikation/config/model/UserData.java
@@ -8,7 +8,7 @@ public class UserData {
     private String username;
     private String password;
     private String role;
-    private static final String FILE_PATH = "user.txt";
+    private static final String FILE_PATH = "/wegfinderProject/user.txt";
 
     public UserData() {
     }
