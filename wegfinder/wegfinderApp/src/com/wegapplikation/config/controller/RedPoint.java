5fb7a44c57dcf001d5d60d0e6f8c678b7cfa5409
Snyxex <153672410+Snyxex@users.noreply.github.com>
2025-11-23 13:04:12 +0100

    main

---
+++ wegfinder/wegfinderApp/src/com/wegapplikation/config/controller/RedPoint.java
@@ -0,0 +1,25 @@
+package com.wegapplikation.config.controller;
+
+public class RedPoint 
+{
+	
+	public RedPoint(String room,  int xpos, int ypos, boolean Open) {
+		this.roomnumber = room;
+		this.x = xpos;
+		this.y = ypos;
+		this.isOpen = Open;
+	}
+
+	public String roomnumber;
+	
+	public int x;
+	
+	public int y;
+	
+	public boolean isOpen;
+	
+	
+
+	
+}
+
